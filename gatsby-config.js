9b8533b0f65f05a9977189f154c333c18b9dae41
Wonkook Lee <wonkook.lee@medistream.co.kr>
2022-06-22 01:28:14 +0900

    Add deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [],
 };
