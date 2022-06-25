c8cba7f4bea34d76051cdacaf84a779ac960aaf0
Wonkook Lee <wonkook.lee@medistream.co.kr>
2022-06-26 02:44:06 +0900

    헤더 제목 변경한다

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
   return (
     <StyledHeader>
       <BlogTitleContainer>
-        <BlogTitle>{`WONKOOK_LEE`}</BlogTitle>
+        <BlogTitle>{`wonkook.dev`}</BlogTitle>
       </BlogTitleContainer>
       <NavContainer>
         <NavList>
           <Nav>
-            <LinkWrapper href='#'>소개</LinkWrapper>
+            <LinkWrapper href="#">소개</LinkWrapper>
           </Nav>
           <Nav>
-            <LinkWrapper href='#'>이력</LinkWrapper>
+            <LinkWrapper href="#">이력</LinkWrapper>
           </Nav>
           <Nav>
-            <LinkWrapper href='#'>블로그</LinkWrapper>
+            <LinkWrapper href="#">블로그</LinkWrapper>
           </Nav>
         </NavList>
       </NavContainer>
     </StyledHeader>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 const StyledHeader = styled.header`
   padding: 0 36px;
@@ -34,27 +34,27 @@ const StyledHeader = styled.header`
   background-color: #fff;
 `;
 
-const BlogTitleContainer = styled.div``
+const BlogTitleContainer = styled.div``;
 
 const BlogTitle = styled.h1`
   font-weight: 600;
   font-size: 28px;
   color: #333;
-`
+`;
 
-const NavContainer = styled.nav``
+const NavContainer = styled.nav``;
 
 const NavList = styled.ul`
   display: flex;
   margin-left: 46px;
-`
+`;
 
 const Nav = styled.li`
   margin-right: 35px;
-`
+`;
 
 const LinkWrapper = styled.a`
   color: #333;
   font-size: 16px;
   cursor: pointer;
-`
+`;
